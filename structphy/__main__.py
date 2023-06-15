385d8ae2c9561ed0f1ec9fc7a53088f1184662a7
Finn O'Donoghue <finnodonoghue@gmail.com>
2023-06-16 01:50:50 +1000

    Manipulate input fasta files for bootstraping then folding

--- structphy/__main__.py
+++ structphy/__main__.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 import click
 import os
-import io
 import multiprocessing
 
 from structphy.install_executables import install_tmalign, install_fastme, install_consense
@@ -28,33 +27,59 @@ def setup_working_dir():
 
     
 @click.command()
-@click.option('-d', '--structdir', type=click.Path(file_okay=False, path_type=Path))
-@click.option('-f', '--fasta', type=click.File(mode='r'))
+@click.option('-d', '--structdir', type=click.Path(file_okay=False, path_type=Path, resolve_path=True))
+@click.option('-f', '--fasta', type=click.Path(exists=True,  path_type=Path, resolve_path=True))
 @click.option('-t', '--threads', type=int, default=multiprocessing.cpu_count())
-@click.option('-n', '--n_bootstraps', type=int, default=3)
-def main(structdir: Path, fasta: io.TextIOWrapper, threads: int, n_bootstraps: int):
+@click.option('-n', '--n_bootstraps', type=int, default=10)
+@click.option('--drop_inserts', is_flag=True, show_default=True, default=False)
+@click.option('--fold_dir', type=click.Path(file_okay=False, path_type=Path, resolve_path=True))
+@click.option('--dropout', type=str)
+def main(structdir: Path, fold_dir: Path, fasta: Path, threads: int, n_bootstraps: int, drop_inserts: bool, dropout: str):
     setup_working_dir()
 
     if (structdir is None) is (fasta is None): #XOR check, has to be one or the other
         raise click.UsageError('Either a directory of structures (--structdir mydir/), OR a fasta file of sequences (--fasta myseqs.fa) needs to be provided.')
     
     if fasta:
-        # TODO process fasta to remove inserts, and create bootstrap copies ID:0 etc.
-        # TODO Make sure output_dir_name is empty or doesnt exist. 
-        # TODO It should be empty ! or it should be a perfect copy of fasta bootstraps
-        
-        # Run inference into 'output_dir_name'
         from structphy.run_inference_docker import run_esm_dropouts
-        fasta_path = Path(fasta.name)
+        from structphy.fasta_loading import fasta_to_dict, fasta_dict_to_bootstrap_string
+
+        # Read fasta file into a dict like {id: sequence, ...}
+        fasta_dict_full, fasta_dict_no_gaps = fasta_to_dict(fasta, drop_inserts)
+        
+        # If a directory for the folding files isn't given make one at ./fastaname_folddir/
+        if not fold_dir:
+            fold_dir = Path(os.getcwd()) / (fasta.name.split('.')[0] + '_folddir')
+            if not os.path.exists(fold_dir):
+                os.mkdir(fold_dir)
+                click.echo(f'Folding bootstraps into {fold_dir}')
+        # Directory has to be empty to avoid mixing up old and new PDBs
+        if os.listdir(fold_dir):
+            raise click.UsageError(f'Tried to use {fold_dir}, but it wasn\'t empty.')
+
+        # Write bootstrap fasta temporarily to the fold_dir
+        bootstrap_fasta_out = fasta_dict_to_bootstrap_string(fasta_dict_no_gaps, n_bootstraps)
+        bootstrap_fasta_path = fold_dir / 'bootstraps.fasta'
+        with open(bootstrap_fasta_path, 'w') as f:
+            f.write(bootstrap_fasta_out)
+
+        
+        # Run inference into 'fold_dir'
+        if dropout:
+            dropout = [float(x) for x in dropout.split(',')]
+
         structdir = run_esm_dropouts( #fasta_in and output_dir_name must be in share_dir for docker!
-            share_dir=fasta_path.parents[0],
-            fasta_in=fasta_path, # file
-            output_dir_name=fasta_path.name.split('.')[0]+'_bootstrap_structs', # TODO replace with click.option
-            dropout=[0.5]*36, #click option
-            max_tokens_per_batch=1300,#click option or maybe auto? not sure
+            share_dir=fold_dir,
+            fasta_in=bootstrap_fasta_path,
+            output_dir_name=fold_dir,
+            dropout=dropout,
+            max_tokens_per_batch=1300, #click option or maybe auto? not sure
         )
-        
-        
+
+        # Remove bootstraps.fa file in fold_dir after structures are made
+        os.remove(bootstrap_fasta_path)
+
+        structdir = fold_dir
  
     structure_files = [(structdir / file).resolve() for file in os.listdir(structdir) if file.endswith('.pdb')]
 
@@ -69,6 +94,7 @@ def main(structdir: Path, fasta: io.TextIOWrapper, threads: int, n_bootstraps: i
 
     # bootstrap against the consensus tree
     print(consensus_tree)
-    # reweight the consensus branch lengths using distance matrices
+    
+    # reweight the consensus branch lengths using distance matrices and optimise routine
 
     # 
---
+++ structphy/fasta_loading.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+from typing import Tuple, Dict
+import re
+
+def fasta_to_dict(fasta_file: Path, drop_inserts: bool) -> Tuple[Dict[str, str], Dict[str, str]]:
+    
+    with open(fasta_file, 'r') as f:
+            fasta_read = f.read()
+    id_seq = ([id_seq.strip() for id_seq in fasta_read.split('>') if id_seq.strip()])
+    id_seq_items = [[split for split in id_seq_pair.split('\n') if split] for id_seq_pair in id_seq]
+    fasta_dict_full = {id_seq_pair[0]:id_seq_pair[1] for id_seq_pair in id_seq_items}
+
+    # Keep gaps ('-') and inserts (lowercase) in fasta_dict
+    # Then remove the gaps and the inserts, but keep them if --drop_inserts
+    fasta_dict_no_gaps = {id: seq.replace('-', '') for id, seq in fasta_dict_full.items()}
+    if not drop_inserts:
+        fasta_dict_no_gaps = {id: seq.upper() for id, seq in fasta_dict_no_gaps.items()}
+    else:
+        fasta_dict_no_gaps = {id: re.sub(r'[a-z]', '', seq) for id, seq in fasta_dict_no_gaps.items()}
+    
+    return fasta_dict_full, fasta_dict_no_gaps
+
+def fasta_dict_to_bootstrap_string(fasta_dict: Dict[str, str], n_bootstraps: int) -> str:
+
+    bootstrap_fasta_out = ''
+    for id, seq in fasta_dict.items():
+        for i in range(n_bootstraps):
+            bootstrap_fasta_out += f'>{id}#{i}\n'
+            bootstrap_fasta_out += seq + '\n\n'
+    
+    return bootstrap_fasta_out
